9867ca19974e25f3d15ee1fb1076cbdc6f4f4311
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-11-20 13:18:48 +0530

    intial set up for the blog app

---
+++ src/Components/BlogTitle/BlogTitle.js
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from "./BlogTitle.module.css";
+
+const BlogTitle =(props)=>{
+
+    return(
+     <div className={classes.title}>
+        <h1>Blog Title</h1>
+        <button type="button">Add New Blog</button>
+        <div className={classes.underline}></div>
+     </div>   
+    )
+}
+
+export default BlogTitle;
+
